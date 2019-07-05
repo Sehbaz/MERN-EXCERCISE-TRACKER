bdb1a70cff025aa4b7fa2fb536df5bc9973dc6da
Sehbaz <sehbazm@gmail.com>
2019-07-05 12:10:46 +0100

    Creating a new user adding page

--- src/components/create-user-component.js
+++ src/components/create-user-component.js
@@ -1,9 +1,57 @@
 import React, { Component } from "react";
 class CreateUser extends Component {
+  constructor(props) {
+    super(props);
+    // Defining "this"
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    //Setting up the state properties
+    this.state = {
+      username: ""
+    };
+  }
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username
+    };
+
+    console.log(user);
+    this.setState({
+      username: ""
+    });
+  }
   render() {
     return (
       <div>
-        <p> I am from CreateUser component</p>
+        <h3>Create New users</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username: </label>
+
+            <input
+              type="text"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+              required
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create user"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
       </div>
     );
   }
