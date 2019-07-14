e22b7e7788ae98c36a10dccb0d4b1abfdda91736
Sehbaz <sehbazm@gmail.com>
2019-07-15 00:09:01 +0100

    Updates

---
+++ precache-manifest.2b8ef74129b188271c4b967105a5ccd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06970cf76c01028d18e87a4f93d397f9",
+    "url": "/MERN-EXCERCISE-TRACKER/index.html"
+  },
+  {
+    "revision": "f8ac17f903fad8994c54",
+    "url": "/MERN-EXCERCISE-TRACKER/static/css/2.118dfa7b.chunk.css"
+  },
+  {
+    "revision": "4bc5d0678e6ce7c55d41",
+    "url": "/MERN-EXCERCISE-TRACKER/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8ac17f903fad8994c54",
+    "url": "/MERN-EXCERCISE-TRACKER/static/js/2.5762c430.chunk.js"
+  },
+  {
+    "revision": "4bc5d0678e6ce7c55d41",
+    "url": "/MERN-EXCERCISE-TRACKER/static/js/main.4d96af6b.chunk.js"
+  },
+  {
+    "revision": "6e3c7bc62e0144ff8de2",
+    "url": "/MERN-EXCERCISE-TRACKER/static/js/runtime~main.731ef3e1.js"
+  }
+]);
